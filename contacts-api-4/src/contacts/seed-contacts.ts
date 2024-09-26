c13c0f4bd7bc988d6913b1d92fbd12f77390ccb9
Muhammad Uzair Rehan <iamsyeduzairshah@gmail.com>
2024-09-27 00:28:11 +0500

    update

---
+++ contacts-api-4/src/contacts/seed-contacts.ts
@@ -0,0 +1,104 @@
+import { Contact } from './entities/contact.entity';
+
+export const SEED_CONTACTS: Contact[] = [
+  {
+    name: { first: 'Anastasios', last: 'Peschel' },
+    login: { uuid: '0a96c966-7ea3-45ea-b402-829fbb6d08b7' },
+    email: 'anastasios.peschel@example.com',
+    picture: {
+      large: 'https://randomuser.me/api/portraits/men/97.jpg',
+      medium: 'https://randomuser.me/api/portraits/med/men/97.jpg',
+      thumbnail: 'https://randomuser.me/api/portraits/thumb/men/97.jpg',
+    },
+  },
+  {
+    name: { first: 'Budimil', last: 'Pirozhenko' },
+    login: { uuid: '58f7d086-485e-401a-b10f-8e9a9f8f22c1' },
+    email: 'budimil.pirozhenko@example.com',
+    picture: {
+      large: 'https://randomuser.me/api/portraits/men/1.jpg',
+      medium: 'https://randomuser.me/api/portraits/med/men/1.jpg',
+      thumbnail: 'https://randomuser.me/api/portraits/thumb/men/1.jpg',
+    },
+  },
+  {
+    name: { first: 'Shchek', last: 'Zhuravel' },
+    login: { uuid: 'e002de54-4bb3-47bb-a901-f3f5a23a28c8' },
+    email: 'shchek.zhuravel@example.com',
+    picture: {
+      large: 'https://randomuser.me/api/portraits/men/45.jpg',
+      medium: 'https://randomuser.me/api/portraits/med/men/45.jpg',
+      thumbnail: 'https://randomuser.me/api/portraits/thumb/men/45.jpg',
+    },
+  },
+  {
+    name: { first: 'Eva', last: 'Jackson' },
+    login: { uuid: '1bb80582-e77c-4bdf-a42e-a3e4ba6f7c57' },
+    email: 'eva.jackson@example.com',
+    picture: {
+      large: 'https://randomuser.me/api/portraits/women/95.jpg',
+      medium: 'https://randomuser.me/api/portraits/med/women/95.jpg',
+      thumbnail: 'https://randomuser.me/api/portraits/thumb/women/95.jpg',
+    },
+  },
+  {
+    name: { first: 'William', last: 'Diaz' },
+    login: { uuid: '67c5a5a6-2c34-415e-b493-f290004a20f6' },
+    email: 'william.diaz@example.com',
+    picture: {
+      large: 'https://randomuser.me/api/portraits/men/55.jpg',
+      medium: 'https://randomuser.me/api/portraits/med/men/55.jpg',
+      thumbnail: 'https://randomuser.me/api/portraits/thumb/men/55.jpg',
+    },
+  },
+  {
+    name: { first: 'Hilla', last: 'Sakala' },
+    login: { uuid: 'e213919c-a70f-43d5-a0e1-9e9227b93d6c' },
+    email: 'hilla.sakala@example.com',
+    picture: {
+      large: 'https://randomuser.me/api/portraits/women/3.jpg',
+      medium: 'https://randomuser.me/api/portraits/med/women/3.jpg',
+      thumbnail: 'https://randomuser.me/api/portraits/thumb/women/3.jpg',
+    },
+  },
+  {
+    name: { first: 'Grace', last: 'Lawson' },
+    login: { uuid: '841ce855-c717-452b-bf4d-6cef304c0c66' },
+    email: 'grace.lawson@example.com',
+    picture: {
+      large: 'https://randomuser.me/api/portraits/women/57.jpg',
+      medium: 'https://randomuser.me/api/portraits/med/women/57.jpg',
+      thumbnail: 'https://randomuser.me/api/portraits/thumb/women/57.jpg',
+    },
+  },
+  {
+    name: { first: 'Vicenta', last: 'Vázquez' },
+    login: { uuid: '2e79ac00-7fc8-40a4-90aa-dd9c9cb8eaee' },
+    email: 'vicenta.vazquez@example.com',
+    picture: {
+      large: 'https://randomuser.me/api/portraits/women/83.jpg',
+      medium: 'https://randomuser.me/api/portraits/med/women/83.jpg',
+      thumbnail: 'https://randomuser.me/api/portraits/thumb/women/83.jpg',
+    },
+  },
+  {
+    name: { first: 'Olivia', last: 'Martin' },
+    login: { uuid: 'b1ca4332-8587-4ec2-a711-00bb7cafc8b1' },
+    email: 'olivia.martin@example.com',
+    picture: {
+      large: 'https://randomuser.me/api/portraits/women/37.jpg',
+      medium: 'https://randomuser.me/api/portraits/med/women/37.jpg',
+      thumbnail: 'https://randomuser.me/api/portraits/thumb/women/37.jpg',
+    },
+  },
+  {
+    name: { first: 'Ceylan', last: 'Paksüt' },
+    login: { uuid: '372b5818-bcdc-478a-af97-59844b4b5728' },
+    email: 'ceylan.paksut@example.com',
+    picture: {
+      large: 'https://randomuser.me/api/portraits/women/60.jpg',
+      medium: 'https://randomuser.me/api/portraits/med/women/60.jpg',
+      thumbnail: 'https://randomuser.me/api/portraits/thumb/women/60.jpg',
+    },
+  },
+];
